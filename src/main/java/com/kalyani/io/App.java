5dbc05160b33f9bdab0112eeb009a724a7dfc8ff
kalyani <kalyani@gmail.com>
2024-10-15 15:12:37 +0530

    To test the webhook now

--- src/main/java/com/kalyani/io/App.java
+++ src/main/java/com/kalyani/io/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "For testing the webhook!" );
+        System.out.println( "For testing the webhook, lets check!" );
     }
 }
